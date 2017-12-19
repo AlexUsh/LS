ee777bb2afc44ef76d7adb27bf8c377f19778d2d
AlexUsh <31797507+AlexUsh@users.noreply.github.com>
2017-12-19 22:56:56 +0300

    Update LS.c

--- LS.c
+++ LS.c
@@ -1,32 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <dirent.h>
-#include <string.h>
 
-int main (int argc, char *argv[])
+
+int main ()
 {
-	DIR *dir;
-	struct dirent * sd;
-	char mfile[127];
-
-	if (argc < 2)
-	{
-		strcpy(mfile, ".");
-	}
-	else
-        strcpy(mfile, argv[1]);
-
-        printf("%s\n\n", mfile);
-        dir = opendir(mfile);
-
-	while((sd = readdir(dir)) != NULL)
-	{
-		printf(">> %s\n", sd->d_name);
-		sd = readdir(dir);
-	}
-
-	closedir(dir);
-
+	
 	return 0;
 }
